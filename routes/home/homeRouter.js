7a0b9cd0b7e8e45e3b65c1343265ee22709bf029
Aditya Vavre <43115743+adityavavre@users.noreply.github.com>
2020-09-02 11:55:02 +0530

    Fixed and improved email verification flow (#4)

--- routes/home/homeRouter.js
+++ routes/home/homeRouter.js
@@ -21,7 +21,7 @@ homeRouter.route("/login")
         if (req.isAuthenticated())
             res.redirect("/platform");
         else
-            res.render("login", { wrongCreds: false });
+            res.render("login", { wrongCreds: false, unverified: false });
     })
     .post(function (req, res, next) {
         passport.authenticate('local', {}, function (err, user, info) {
@@ -31,7 +31,11 @@ homeRouter.route("/login")
             }
             if(!user){
                 console.log(info);
-                return res.render("login", { wrongCreds: true }); 
+                return res.render("login", { wrongCreds: true, unverified: false });
+            }
+            // check if user is active i.e. college email id is verified or not
+            if(!user.active){
+                return res.render("login", { wrongCreds: false, unverified: true });
             }
             req.logIn(user, function (err) {
                 if(err){
@@ -73,36 +77,45 @@ homeRouter.route('/signup')
             console.log(err);
             return;
         }
+        // return back to signup page and highlight email box saying email is already in use
         if(result && result.active){
+            //TODO(aditya)
             return res.end("<h1>Email has already been registered with us</h1>");
         }
-        // if email verification has not been done
+        // if email verification has not been done, remove existing doc in collection and allow signup again
         else if(result){
-            //TODO(aditya): Add functionality to allow user to signup again/send verification link again
-            return res.end("<h1>Email has been registered but only needs to be verified</h1>");
-        }
-        // if email is not already in database, add new user to database
-        else {
-            const finalUser = new Users({
-                email: req.body.c_email,
-                password: req.body.password,
-                active: false
+            Users.deleteOne({ _id: result._id}, function(err, obj) {
+                if(err){
+                    console.log("Failed to remove entry from collection")
+                    console.log(err);
+                    //TODO(aditya): Handle error properly
+                    return res.render('signup', {name: "", email: ""});
+                }
+                console.log("1 document deleted");
             });
-
-            // set password
-            finalUser.setPassword(req.body.password);
-
-            // set verification hash and get the token for email
-            let token = finalUser.setVerifyHash();
-
-            // add user to database and send a verification email
-            return finalUser.save()
-                .then(() => {
-                    //TODO(aditya): Not sure what to do with the 'r' here
-                    sendVerificationEmail(req.body.c_email, req.body.name, token).then(r => console.log("promise", r));
-                    res.json({ user: finalUser.toAuthJSON() });
-                });
         }
+        // add user to database
+        const finalUser = new Users({
+            email: req.body.c_email,
+            password: req.body.password,
+            active: false
+        });
+
+        // set password
+        finalUser.setPassword(req.body.password);
+
+        // set verification hash and get the token for email
+        let token = finalUser.setVerifyHash();
+
+        // add user to database and send a verification email
+        return finalUser.save()
+            .then(() => {
+                //TODO(aditya): Not sure what to do with the 'r' here
+                sendVerificationEmail(req.body.c_email, req.body.name, token).then(r => console.log("promise", r));
+                // res.json({ user: finalUser.toAuthJSON() });
+                //TODO(aditya): Make a webpage for this. Maybe with a resend verification link?
+                res.end("<h1>A verification link has been sent to your college email id. Please verify it to continue to our platform.</h1>");
+            });
     });
 });
 
@@ -121,10 +134,12 @@ homeRouter.get('/verify',function(req,res){
                 return;
             }
             // link verified for user
+            //TODO(aditya): remove verifyHash from document once a user is verified
             Users.findByIdAndUpdate(
                 {_id: user._id},
                 {
-                    "active": true
+                    $unset: {verifyHash: 1},
+                    $set: {active: true}
                 },
                 { useFindAndModify: false },
                 function(err, result){
@@ -136,7 +151,7 @@ homeRouter.get('/verify',function(req,res){
                     }
                 })
             console.log("Email has been verified");
-            res.end("<h1>Email has been Successfully verified");
+            return res.redirect('/platform');
         })
 });
 
