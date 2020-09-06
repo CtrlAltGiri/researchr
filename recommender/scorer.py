49b479b50d11f890a6e7fd39958a74da8494c566
adityavave <aditya.vavre@gmail.com>
2020-09-06 13:18:45 +0530

    Adding filter functionality for recommending projects and fetching MongoURL from env file

--- recommender/scorer.py
+++ recommender/scorer.py
@@ -1,6 +1,7 @@
 from typing import List, Set
 
-def calc_score(num_interest_match, num_exp_match, w1=0.7, w2=0.3):
+
+def calc_score(num_interest_match: int, num_exp_match: int, w1=0.7, w2=0.3):
     """
     Function to calculate recommender score of a project based on interest and exp. match
     :param num_interest_match: Number of interest tags that matched
@@ -9,7 +10,7 @@ def calc_score(num_interest_match, num_exp_match, w1=0.7, w2=0.3):
     :param w2: Weight of score to be assigned to exp. match
     :return: score
     """
-    return (w1*num_interest_match)+(w2*num_exp_match)
+    return (w1 * num_interest_match) + (w2 * num_exp_match)
 
 
 def recommend(projects: List, interest_tags: Set, work_exp_tags: Set):
@@ -27,4 +28,3 @@ def recommend(projects: List, interest_tags: Set, work_exp_tags: Set):
     # sort first based on score and then views
     recommended_projects = [i[0] for i in sorted(scored_projects, key=lambda x: (x[1], x[0]['views']), reverse=True)]
     return recommended_projects, len(recommended_projects)
-
