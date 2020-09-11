064cf3ceaeaee765328f21513ede0031f32b1bbd
Aditya Vavre <43115743+adityavavre@users.noreply.github.com>
2020-09-11 12:25:35 +0530

    Adding professors schema (#24)

--- models/professors.js
+++ models/professors.js
@@ -1,19 +1,15 @@
 const mongoose = require('mongoose');
 const crypto = require('crypto');
 
-const StudentsSchema = new mongoose.Schema({
+const ProfessorsSchema = new mongoose.Schema({
     name: String,
     college: String,
-    degree: String,
-    branch: String,
-    yog: Number,
     doj: Date,
     lastLogin: Date,
     c_email: String,
-    p_email: String,
-    cvElements: Object,
-    completed: Boolean,
-    TandC: Boolean,
+    designation: String,
+    completed: Boolean, //completed his profile?
+    TandC: Boolean, //TandC for prof?
     hash: String,
     salt: String,
     active: Boolean,
@@ -22,28 +18,28 @@ const StudentsSchema = new mongoose.Schema({
     resetExpires: Date
 });
 
-StudentsSchema.methods.setPassword = function(password) {
+ProfessorsSchema.methods.setPassword = function(password) {
     this.salt = crypto.randomBytes(16).toString('hex');
     this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
 };
 
-StudentsSchema.methods.setVerifyHash = function() {
+ProfessorsSchema.methods.setVerifyHash = function() {
     this.verifyHash = crypto.randomBytes(128).toString('hex');
     return this.verifyHash;
 };
 
 // setting hash for reset password flow
-StudentsSchema.methods.setResetHash = function() {
+ProfessorsSchema.methods.setResetHash = function() {
     this.resetHash = crypto.randomBytes(128).toString('hex');
     this.resetExpires = Date.now() + 3600000; // 1 hour
     return this.resetHash;
 };
 
-StudentsSchema.methods.validatePassword = function(password) {
+ProfessorsSchema.methods.validatePassword = function(password) {
     const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
     return this.hash === hash;
 };
 
-const Students = mongoose.model('students', StudentsSchema, 'students');
+const Professors = mongoose.model('professors', ProfessorsSchema, 'professors');
 
-module.exports = Students;
+module.exports = Professors;
