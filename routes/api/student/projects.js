7809107c6f541669b5bac4d42a163d308b2aabd8
Aditya Vavre <43115743+adityavavre@users.noreply.github.com>
2020-09-15 12:53:53 +0530

    Adding restricted view and college view filter (#37)

--- routes/api/student/projects.js
+++ routes/api/student/projects.js
@@ -4,14 +4,35 @@ const ProfProjects = require('../../../models/profProjects');
 // API to show all projects on the main platform page to the students
 projectsRouter.route("/")
     .get(function (req, res) {
-        // query all prof projects with application close date > cur date
-        ProfProjects.find({applicationCloseDate: {$gt: Date.now()}}, function (err, projects){
+        console.log("Here")
+        // following variable decides if college view is to be shown or a normal view is to be shown to the viewer
+        let showAllProjects = req.query.allProjects
+        // get student's college name from req
+        let studentCollege = req.user.college
+
+        // construct the filter query
+        let filter = {applicationCloseDate: {$gt: Date.now()}}
+
+        if(showAllProjects === "false"){
+            filter.college = studentCollege;
+        }
+
+        // query all prof projects with application close date > cur date and based on the view
+        ProfProjects.find(filter, function (err, projects){
             if(err){
                 console.log(err);
                 return res.status(404).send("Failed");
             }
-            // send all projects to front end
-            return res.status(200).send(projects);
+            else{
+                // filter all restricted projects that are not in same college
+                projects = projects.filter(function (element){
+                    return (element.restrictedView !== true || (element.restrictedView === true && element.college === studentCollege));
+                })
+
+                // send all projects to front end
+                return res.status(200).send(projects);
+            }
+
         })
     })
 
