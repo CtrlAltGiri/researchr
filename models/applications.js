dda4b9cf9c3fc56a0945fb0bd0a6b4d33f1740ad
Aditya Vavre <43115743+adityavavre@users.noreply.github.com>
2020-09-20 21:01:09 +0530

    Adding /applications/:projectID API to view all applicants for a project (#51)

--- models/applications.js
+++ models/applications.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 
 const ApplicationsSchema = new mongoose.Schema({
-    name: String,
-    college: String,
-    branch: String,
+    name: String, // student name
+    college: String, // student college
+    branch: String, // student branch
     profApplications: [
         {
             projectID: {type: mongoose.Types.ObjectId, unique: true},
-            name: String,
+            name: String, // project name
             sameCollege: Boolean, // If the project offered is from the same college
-            cgpa: Number,
+            cgpa: Number, // student cgpa at time of application
             professorName: String,
             status: String,
-            doa: Date,
+            doa: Date, // date of application
             answers: [ String ],
             sop: String,
             professorMsg: String,
