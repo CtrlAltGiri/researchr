a5f71f23ac4e823224134f5294839914fa273261
Giridhar <giridhar.balaz@gmail.com>
2020-10-12 09:50:55 +0530

    Stupid mistake

--- client/src/components/App/Error.js
+++ client/src/components/App/Error.js
@@ -1,25 +1,9 @@
-import { BandwidthLimitExceeded } from 'http-errors';
 import React, { Component, useRef, useState } from 'react'
-import DatePicker from 'react-date-picker/dist/entry.nostyle';
-import '../../assets/datepicker.css'
 
 
 function Error(props){
 
-    return <MyApp />
+    return <h1> Error </h1>
 }
-
-function MyApp() {
-    const [value, onChange] = useState(new Date());
-  
-    return (
-      <div>
-        <DatePicker
-          onChange={onChange}
-          value={value}
-        />
-      </div>
-    );
-  }
 
 export default Error;
