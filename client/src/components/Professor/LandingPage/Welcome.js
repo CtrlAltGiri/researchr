4cb07e99eb113a8c935ce0cd0b8d5cb03e2d27f1
Giridhar <giridhar.balaz@gmail.com>
2020-10-19 15:19:42 +0530

    Text changes

--- client/src/components/Professor/LandingPage/Welcome.js
+++ client/src/components/Professor/LandingPage/Welcome.js
@@ -5,10 +5,10 @@ function Welcome(props){
     return (
         <div className="px-8 md:px-16">
             <h1 className="text-3xl font-bold">Welcome to researchR</h1>
-            <p className="mb-12">To get you started, there a couple of things</p>
+            <p className="mb-12">To get you started, there are a couple of things</p>
             <div className="px-4 md:px-8">
                 <p className="text-xl font-thin my-2">1. Create your profile in My Profile (so that students can view it)</p>
-                <p className="text-xl font-thin my-2">2. Add Project(s) using the Add Project (+) in this page.</p>
+                <p className="text-xl font-thin my-2">2. Add Project(s) using the Add Project (+) button on this page.</p>
             </div>
         </div>
     )
